661c04dfa013d8f3fdf031b3a5da36e83dda76ba
unknown <isaraluk.p@gmail.com>
2020-10-04 08:37:02 +0700

    First Commit

---
+++ functions/dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+    {
+        userHandle: 'user',
+        body:'this is the scream body',
+        createdAt:"2020-10-03T20:44:44.165Z",
+        likeCount: 5,
+        commentCount: 2
+    }
+    ]
+}
