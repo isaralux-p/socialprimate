e8f017ba09a5ad340860a7e6fc2accb494bd949f
isaraluk-p <isaraluk.p@github.com>
2020-10-05 13:09:12 +0700

    Validation 1
    add validation section to app

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,30 @@ const functions = require('firebase-functions');
 
 admin.initializeApp();
 
-const express = require('express');
-const app =express();
+
+//--------------------- config to firebase project ---------------------//
+const config = {
+   apiKey: "AIzaSyC_AQJXt4xr1ckcZPPzDDt45R-Yxmg5yGE",
+   authDomain: "mysocialprimate.firebaseapp.com",
+   databaseURL: "https://mysocialprimate.firebaseio.com",
+   projectId: "mysocialprimate",
+   storageBucket: "mysocialprimate.appspot.com",
+   messagingSenderId: "828121670867",
+   appId: "1:828121670867:web:05b5ee8919023bc17c2026",
+   measurementId: "G-MT03S1LQKD"
+ };
+
+//-------------------------- initial section --------------------------//
+const app = require('express')();
+const firebase = require('firebase');
+const db = admin.firestore();
+
+firebase.initializeApp(config);
 
 
+// get collection route
 app.get('/screams', (req,res) => {
-   admin
-   .firestore()
+   db
    .collection('screams')
    .orderBy('createdAt', 'desc')
    .get()
@@ -28,7 +45,7 @@ app.get('/screams', (req,res) => {
    .catch(err => console.error(err));
 });
 
-
+//---------------------  add collection route  ---------------------//
 app.post('/scream',(req,res) =>{
 
    const newScream = {
@@ -37,7 +54,7 @@ app.post('/scream',(req,res) =>{
       createdAt: new Date().toISOString()
    };
 
-   admin.firestore()
+      db
       .collection('screams')
       .add(newScream)
       .then(doc => {
@@ -48,5 +65,59 @@ app.post('/scream',(req,res) =>{
          console.error(err);
       })
 })
+
+//---------------------  Signup Route  ---------------------//
+
+app.post ('/signup', (req,res) => {
+   const newUser = {
+      email : req.body.email,
+      password : req.body.password,
+      confirmPassword : req.body.confirmPassword,
+      handle : req.body.handle
+   };
+
+
+   //---------------------  Validated Section  ---------------------//
+
+   let tokenId,userId;
+   db.doc(`/users/${newUser.handle}`)
+   .get()
+   .then(doc => {
+      if(doc.exists){
+         return res.status(400).json({ handle: 'this handle is already taken'});
+      }
+      else {
+         return firebase
+         .auth()
+         .createUserWithEmailAndPassword(newUser.email, newUser.password)   
+      }
+   })
+   .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+   })
+   .then(token => {
+      tokenId = token
+      const userCredentials = {
+         handle: newUser.handle,
+         email: newUser.email,
+         createdAt: new Date().toISOString(),
+         userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials); 
+   })
+   .then (() =>{
+      return res.status(201).json({ tokenId });
+   })
+   .catch (err => {
+      if(err.code === "auth/email-already-in-use"){
+         return res.status(400).json({ email: 'Email is already taken!'});
+      }
+      else {
+         console.error(err);
+         return res.status(500).json({ error: err.code });
+      }    
+   })
+});
 
 exports.api = functions.https.onRequest(app);
